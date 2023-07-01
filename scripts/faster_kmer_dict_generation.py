2f8735c02ee3c3e8d36dcbf14909c325b52fb758
Divya Shridar <56643379+dibsondivya@users.noreply.github.com>
2023-07-01 04:33:54 +0100

    optimised runtime ~1hr

--- scripts/faster_kmer_dict_generation.py
+++ scripts/faster_kmer_dict_generation.py
@@ -1,69 +1,75 @@
 import sys
 import subprocess
-import pandas as pd
 import os
+import csv
 from multiprocessing import Pool as ThreadPool  
 import collections
 from tqdm import tqdm
-from tqdm.contrib.concurrent import process_map  # or thread_map
 
 
-kmer_size = 21 # ACTIONABLE: to be declared
-
+kmer_size = 31 # ACTIONABLE: to be declared
 os.chdir('/rds/general/user/dds122/ephemeral/kmer_'+str(kmer_size)+'_txt/')
-
-# import data containing accession | ribotype of all top 10 ribotype samples
-df = pd.read_csv('/rds/general/project/hda-22-23/live/Summer_projects/dds122/data/05-25-2023/noNA_downsized.csv')
-n_threads = 1
+n_threads = 4
 
 # load library kmers
 global kmer_2_id
-kmer_2_id = pd.read_csv('/rds/general/project/hda-22-23/live/Summer_projects/dds122/data/05-25-2023/kmer_id21.csv')
+kmer_2_id = dict()
+file_content = csv.reader(open('/rds/general/project/hda-22-23/live/Summer_projects/dds122/data/05-25-2023/kmer_id'+str(kmer_size)+'.csv'), delimiter=',')
+for line in file_content:
+    kmer = line[0]
+    id_ = line[1]
+    kmer_2_id[kmer] = id_
+    
+
 
 def create_csv(file):
     id_count = collections.defaultdict(int)
 
     accession = file.replace('_1.txt', '')
-
+    
     output = open(file, "r")
     Lines = output.readlines()
     for line in Lines:
         insert = line.strip().split('\t')
         if insert and len(insert) == 2:
             kmer = insert[0]
-            if kmer in kmer_2_id['kmer'].tolist():
-                id_ = int(kmer_2_id[kmer_2_id['kmer']==kmer]['id'])
+            if kmer in kmer_2_id:
+                id_ = kmer_2_id[kmer]
                 id_count[id_] += int(insert[1])
+    
+    try:
+        output = open(file.replace('_1.txt','_2.txt'), "r")
+        Lines = output.readlines()
+        for line in Lines:
+            insert = line.strip().split('\t')
+            if insert and len(insert) == 2:
+                kmer = insert[0]
+                if kmer in kmer_2_id:
+                    id_ = kmer_2_id[kmer]
+                    id_count[id_] += int(insert[1])
+    except:
+        print('Error with file ' + file.replace('_1.txt','_2.txt'))
 
-    output = open(file.replace('_1.txt','_2.txt'), "r")
-    Lines = output.readlines()
-    for line in Lines:
-        insert = line.strip().split('\t')
-        if insert and len(insert) == 2:
-            kmer = insert[0]
-            if kmer in kmer_2_id['kmer'].tolist():
-                id_ = int(kmer_2_id[kmer_2_id['kmer']==kmer]['id'])
-                id_count[id_] += int(insert[1])
-    
-    
-    out = open('/rds/general/user/dds122/ephemeral/kmer_21_df/' + accession + '.txt', 'w+')
+    out = open('/rds/general/user/dds122/ephemeral/kmer_'+str(kmer_size)+'_df/' + accession + '.txt', 'w+')
     for id_, count_ in id_count.items():
         out.write(str(id_) + '	' + str(count_) + '\n')
     out.close()
+
     
     pbar.update(1)
     
    
 file_list = []
-for file in os.listdir('/rds/general/user/dds122/ephemeral/kmer_21_txt/'):
+for file in os.listdir('/rds/general/user/dds122/ephemeral/kmer_'+str(kmer_size)+'_txt/'):
     if file.endswith('_1.txt'):
         file_list.append(file)
 
 pbar = tqdm(total=len(file_list))
 
+
 ## process samples        
 pool = ThreadPool(n_threads) 
-tmp_res = pool.map_async(create_csv, file_list, chunksize=None)
+tmp_res = pool.map_async(create_csv, file_list)
 output = tmp_res.get()
 pool.close() 
 pool.join() 
