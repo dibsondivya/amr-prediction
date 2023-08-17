61b070fad6803bedfcd5c324bfff9af555fe5cd0
Divya Shridar <56643379+dibsondivya@users.noreply.github.com>
2023-08-17 17:03:30 +0100

    Update 4_read file kmer counting.py

--- scripts/2_data preprocessing/4_read file kmer counting.py
+++ scripts/2_data preprocessing/4_read file kmer counting.py
@@ -6,7 +6,7 @@ from multiprocessing import Pool as ThreadPool
 
 df = pd.read_csv('/rds/general/project/hda-22-23/live/Summer_projects/dds122/data/05-25-2023/noNA_downsized.csv')
 # subset to only top three ribotypes
-df = df[(df['ribotype'] == 27) | (df['ribotype'] == 78) | (df['ribotype'] == 14)]
+# df = df[(df['ribotype'] == 27) | (df['ribotype'] == 78) | (df['ribotype'] == 14)]
 SRA_ID_list = df['accession'].tolist()
 
 sample_id = 0
@@ -15,7 +15,7 @@ id_count = {}
 id_sample = {}
 id_ribotype = {}
 
-kmer_size = 21 # ACTIONABLE: to be changed as required; 11, 17, 21 and 31
+kmer_size = 21 # ACTIONABLE: to be changed as required; 17, 21 and 31
 n_threads = 1
 
 def mapping(accession):
